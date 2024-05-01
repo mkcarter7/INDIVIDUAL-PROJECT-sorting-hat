5ca4c1585150f60903824e7ad3d715d9c09aff52
Melissa Carter <mkd.princess@gmail.com>
2024-04-30 19:40:40 -0500

    corrected js errors

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 // STUDENT CARD ARRAY
-const students = [
-  id: 1,
-  student: ,
-  house: ,
-]
+// const students = [
+  // id: 1,
+  // student: ,
+  // house: ,
+// ]
 
-// MAIN PAGE BUTTON
-document.getElementById('bnt1');
-bnt1.addEventListener("click")function(){
+// MAIN PAGE BUTTON AND MODAL
+const bnt1 = document.getElementById('bnt1');
+bnt1.addEventListener('click'),
+function() {
 document.querySelector('.bg-modal').style.display = 'flex';
-}
-
-
-// FUNCTIONS
+};
+document.queerySelector('close').addEventListener('click'),
+function() {
+  document.querySelector('bg-modal').style.dispay = 'none';
+};
 
 // HOUSE
 function assignHouse() {
-  const house = ['Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin'];
-  return house[Math.floor](Math.random() * house.length)];
+  const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin'];
+  return houses[Math.floor(Math.random() * houses.length)];
 }
 
 //  ADD STUDENT CARD
@@ -27,25 +29,19 @@ function createStudentCard(student) {
   card.classList.add('form', 'mb-3');
 const cardText = document.createElement('p');
 cardText.classList.add('card-text');
-cardText.textContent = ('house: $(student.house'});
+cardText.textContent = 'house: ${student.house}';
 }
+card.appendChild(cardText);
 
 
-
+// CARDS ON DOM
 
 const cardsOnDom = (students) => {
   let domString = "";
-  for (const student of students) }
-  domString +=${student.house}""
-
-
-
-
-// CARDS ON THE DOM
-
-
-
-// FUNCTION TO FILTER
+  for (const student of students) {
+    domString += '${student.house}';
+  }
+  
 
 
 
